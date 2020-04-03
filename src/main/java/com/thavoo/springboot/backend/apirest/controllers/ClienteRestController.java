76449c8781ecf49aaebd060d068577622d896380
Gustavo Herrera <gustavoh.2312@gmail.com>
2020-04-03 07:15:16 -0500

    aplicando validaciones y manejo de errores en el crear y actualizar

--- src/main/java/com/thavoo/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/thavoo/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,14 +1,20 @@
 package com.thavoo.springboot.backend.apirest.controllers;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -61,10 +67,23 @@ public class ClienteRestController {
 	
 	@PostMapping("/clientes")
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente,
+			BindingResult result) {
 		
 		Cliente clienteNew = null;
 		Map<String, Object> response = new HashMap<>();
+		
+
+		if(result.hasErrors()) {
+		
+			List<String> errors = result.getFieldErrors()
+									.stream()
+									.map(err -> "El campo '" + err.getField() +"' " +err.getDefaultMessage()
+									).collect(Collectors.toList());
+			
+			response.put("error", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
 		
 		try {
 			clienteNew = clienteService.save(cliente);
@@ -84,11 +103,22 @@ public class ClienteRestController {
 	
 	
 	@PutMapping("/clientes/{id}")
-	public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Cliente cliente) {
+	public ResponseEntity<?> update(@Valid @PathVariable Long id, @RequestBody Cliente cliente, BindingResult result) {
 		
 		Cliente clienteActual = clienteService.findById(id);
 		Cliente clienteUpdated = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			
+			List<String> errors = result.getFieldErrors()
+									.stream()
+									.map(err -> "El campo '" + err.getField() +"' " +err.getDefaultMessage()
+									).collect(Collectors.toList());
+			
+			response.put("error", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
 		
 		if(clienteActual == null) {
 			response.put("mensaje", "El cliente ID: " .concat(id.toString().concat(" no exite en la base de datos")));
